15b0db3f7af7d416972f9d8fd6e40de021e1cf8e
YukiYouda <youda0518@yahoo.co.jp>
2021-09-29 16:50:35 +0900

    編集画面と新規登録画面修正

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', '編集')
 
 @section('content')
-    <div class="m-4">
+    <div class="m-4 text-center">
         <h1>編集画面</h1>
         @if ($errors->any())
             <div class="error">
@@ -21,27 +21,27 @@
         <form action="{{ route('customers.update', $customer) }}" method="post">
             @csrf
             @method('PATCH')
-            <div class="mt-3 form-group">
+            <div class="mt-3 form-group justify-content-center row" >
                 <label for="name">名前</label>
                 <input type="text" class="form-control w15" name="name" id="name"
                     value="{{ old('name', $customer->name) }}">
             </div>
-            <div class="email mt-2 form-group">
+            <div class="email mt-2 form-group justify-content-center row">
                 <label for="email">メールアドレス</label>
-                <input type="email" class="form-control w15" name="email" id="email"
+                <input type="email" class="form-control w20" name="email" id="email"
                     value="{{ old('email', $customer->email) }}">
             </div>
-            <div class="postcode mt-2 form-group">
+            <div class="postcode mt-2 form-group justify-content-center row">
                 <label for="postcode">郵便番号</label>
                 <input type="text" class="form-control w15" name="postcode" id="postcode"
                     value="{{ old('postcode', $customer->postcode) }}">
             </div>
-            <div class="address mt-2 form-group">
+            <div class="address mt-2 form-group justify-content-center row">
                 <label for="address">住所</label>
                 <textarea name="address" class="form-control textarea" id="address" cols="30"
                     rows="10">{{ old('address', $customer->address) }}</textarea>
             </div>
-            <div class="phone_number mt-2 form-group">
+            <div class="phone_number mt-2 form-group justify-content-center row">
                 <label for="phone_number">電話番号</label>
                 <input type="text" class="form-control w15" name="phone_number" id="phone_number"
                     value="{{ old('phone_number', $customer->phone_number) }}">
--- resources/views/customers/register.blade.php
+++ resources/views/customers/register.blade.php
@@ -3,7 +3,7 @@
 @section('title', '新規登録')
 
 @section('content')
-    <div class="body m-4">
+    <div class="m-4 text-center">
         <h1>新規登録画面</h1>
         @if ($errors->any())
             <div class="error">
@@ -21,23 +21,23 @@
 
         <form action="{{ route('customers.store') }}" method="post">
             @csrf
-            <div class="name mt-3 form-group">
+            <div class="name mt-3 form-group justify-content-center row">
                 <label for="name">名前</label>
                 <input type="text" class="form-control w15" name="name" id="name" value="{{ old('name') }}">
             </div>
-            <div class="email mt-2 form-group">
+            <div class="email mt-2 form-group justify-content-center row">
                 <label for="email">メールアドレス</label>
-                <input type="email" class="form-control w15" name="email" id="email" value="{{ old('email') }}">
+                <input type="email" class="form-control w20" name="email" id="email" value="{{ old('email') }}">
             </div>
-            <div class="postcode mt-2 form-group">
+            <div class="postcode mt-2 form-group justify-content-center row">
                 <label for="postcode">郵便番号</label>
                 <input type="text" class="form-control w15" name="postcode" id="postcode" value="{{ $zipcode }}">
             </div>
-            <div class="address mt-2 form-group">
+            <div class="address mt-2 form-group justify-content-center row">
                 <label for="address">住所</label>
                 <textarea name="address" class="form-control textarea" id="address" rows="5">{{ $address }}</textarea>
             </div>
-            <div class="phone_number mt-2 form-group">
+            <div class="phone_number mt-2 form-group justify-content-center row">
                 <label for="phone_number">電話番号</label>
                 <input type="text" class="form-control w15" name="phone_number" id="phone_number"
                     value="{{ old('phone_number') }}">
